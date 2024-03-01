6d594b3832882287248230fa87e31c8db5803d1f
Lorenzo Minciotti <loriminciotti@MacBook-Pro-di-Lorenzo.local>
2024-03-01 15:08:58 +0100

    Update consent command on every page

--- _dev/src/gtag-consent-init.js
+++ _dev/src/gtag-consent-init.js
@@ -1,7 +1,7 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() { window.dataLayer.push(arguments); }
 
-['cc:onFirstConsent'].forEach((eventName) => {
+['cc:onConsent'].forEach((eventName) => {
     window.addEventListener(eventName, (e) => {
         let consent = {}
         let userPreferences = CookieConsent.getUserPreferences()
@@ -16,6 +16,11 @@ function gtag() { window.dataLayer.push(arguments); }
         // analytics storage force
         consent['analytics_storage'] = 'granted'
 
+        // Consent mode v2
+        // ad_personalization & ad_user_data follow ad_storage value
+        consent['ad_personalization'] = consent['ad_storage']
+        consent['ad_user_data'] = consent['ad_storage']
+
         gtag('consent', 'update', consent)
     })
 })
@@ -25,10 +30,15 @@ window.addEventListener('cc:onChange', function(event){
     let userPreferences = CookieConsent.getUserPreferences()
 
     event.detail.changedCategories.forEach(category => {
-        if(category == 'analytics_storage') { // analytics storage force
-            consent[category] = 'granted'    
+        if(category === 'analytics_storage') { // analytics storage force
+            consent[category] = 'granted'
         } else {
             consent[category] = userPreferences.acceptedCategories.includes(category) ? 'granted' : 'denied'
+        }
+
+        if(category === 'ad_storage') {
+            consent['ad_personalization'] = consent['ad_storage']
+            consent['ad_user_data'] = consent['ad_storage']
         }
     });
 
